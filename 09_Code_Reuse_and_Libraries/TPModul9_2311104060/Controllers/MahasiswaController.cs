11c6e601b5b08f5b887ad2159a3c33e92a936d17
“auliajsf06” <auliajasifa24@gmail.com>
2025-04-24 22:57:09 +0700

    Update

---
+++ 09_Code_Reuse_and_Libraries/TPModul9_2311104060/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPModul9_2311104060.Models;
+
+namespace TPModul9_2311104060.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Aulia Jasifa", Nim = "2311104060" },
+            new Mahasiswa { Nama = "Reyner Atira", Nim = "2311104057" },
+            new Mahasiswa { Nama = "Ilham Lii", Nim = "2311104068" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetAll()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+            return mahasiswaList[index];
+        }
+
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mhs)
+        {
+            mahasiswaList.Add(mhs);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+                return NotFound();
+
+            mahasiswaList.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ 09_Code_Reuse_and_Libraries/TPModul9_2311104060/Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace TPModul9_2311104060.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
