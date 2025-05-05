77773bdc83653124d83f92a4c7cafed5ccda2df5
“auliajsf06” <auliajasifa24@gmail.com>
2025-05-05 22:02:17 +0700

    JURNAL10_2311104060

---
+++ 10_Integrated_Project_Implementation/JURNAL10_2311104060/ConsoleAppMatematika/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MatematikaLibraries;
+
+namespace ConsoleAppMatematika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FPB(60, 45): " + Matematika.FPB(60, 45)); 
+            Console.WriteLine("KPK(12, 8): " + Matematika.KPK(12, 8));   
+
+            int[] persamaan = { 1, 4, -12, 9 };
+            Console.WriteLine("Turunan({1,4,-12,9}): " + Matematika.Turunan(persamaan)); 
+
+            int[] integral = { 4, 6, -12, 9 };
+            Console.WriteLine("Integral({4,6,-12,9}): " + Matematika.Integral(integral)); 
+        }
+    }
+}
---
+++ 10_Integrated_Project_Implementation/JURNAL10_2311104060/MatematikaLibraries/Class1.cs
@@ -0,0 +1,61 @@
+﻿namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        // A. FPB
+        public static int FPB(int input1, int input2)
+        {
+            while (input2 != 0)
+            {
+                int temp = input2;
+                input2 = input1 % input2;
+                input1 = temp;
+            }
+            return input1;
+        }
+
+        // B. KPK
+        public static int KPK(int input1, int input2)
+        {
+            return (input1 * input2) / FPB(input1, input2);
+        }
+
+        // C. Turunan
+        public static string Turunan(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int pangkat = persamaan.Length - i - 1;
+                int koef = persamaan[i] * pangkat;
+
+                if (koef == 0) continue;
+
+                string suku = koef.ToString();
+                if (pangkat - 1 > 1)
+                    suku += $"x^{pangkat - 1}";
+                else if (pangkat - 1 == 1)
+                    suku += "x";
+
+                hasil.Add(suku);
+            }
+            return string.Join(" + ", hasil);
+        }
+
+        // D. Integral
+        public static string Integral(int[] persamaan)
+        {
+            List<string> hasil = new List<string>();
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                int pangkat = persamaan.Length - i;
+                double koef = (double)persamaan[i] / pangkat;
+
+                string suku = $"{koef}x^{pangkat}";
+                hasil.Add(suku);
+            }
+            hasil.Add("C");
+            return string.Join(" + ", hasil);
+        }
+    }
+}
